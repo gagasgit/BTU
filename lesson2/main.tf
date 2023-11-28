bbabdab91230b6254df7293f03de899215031ee9
gagasgit <95085194+gagasgit@users.noreply.github.com>
2023-11-28 13:37:23 +0400

    Update main.tf

--- lesson2/main.tf
+++ lesson2/main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-#provider "aws" {
-#  region = "us-east-1"
-#}
-
-
 resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 data "aws_ami" "latest_amazon_linux" {
@@ -33,7 +28,36 @@ resource "aws_instance" "my_server" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.my_server.id]
   monitoring             = var.enable_detailed_monitoring
-  user_data              = file("server_cms.sh")
+ # user_data              = file("server_cms.sh")
+user_data = <<-EOF
+  #!/bin/bash
+  sudo yum -y update
+  sudo yum -y install httpd
+  sudo yum install -y mysql
+  sudo amazon-linux-extras enable php7.4
+  sudo yum clean metadata
+  sudo yum install -y php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap,devel}
+  sudo usermod -a -G apache ec2-user
+  sudo chown -R ec2-user:apache /var/www
+  sudo chmod 2775 /var/www
+  sudo find /var/www -type d -exec chmod 2775 {} \;
+  sudo find /var/www -type f -exec chmod 0664 {} \;
+  sudo mkdir -p /var/www/inc
+  sudo cat <<EOT > /var/www/inc/dbinfo.inc
+  <?php
+  define('DB_SERVER', '${aws_db_instance.db.address}');
+  define('DB_USERNAME', 'webapp');
+  define('DB_PASSWORD', '${random_string.rds_password.result}');
+  define('DB_DATABASE', 'appmariadb');
+  ?>
+  EOT
+  cd /var/www/html
+  wget https://raw.githubusercontent.com/gagasgit/DATABASE_Check_APP/main/index.php
+  sudo service httpd start
+  chkconfig httpd on
+EOF
+
+  depends_on = [aws_db_instance.db]
   tags = merge(var.common_tags, { Name = "${var.common_tags["Environment"]} Server Build by Terraform" })
 
 }
@@ -80,5 +104,13 @@ output "rds_password" {
   value = data.aws_ssm_parameter.rds_password.value
   sensitive = true
 }
+
+# output "DB_Instance_Endpoint" {
+#     value = aws_db_instance.db
+# }
+
+# output "DB_Instance_Status" {
+#     value = aws_db_instance.db
+# }
 
 #terraform taint aws_instance.my_server
